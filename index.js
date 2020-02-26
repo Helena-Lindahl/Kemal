eae170b50bf5a72471b77fcf3c5d9fee695e4dab
Helena-Lindahl <helena.l.lindahl@gmail.com>
2020-02-26 11:41:11 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// Härifrån startar vi upp vår webshop
+const mongoose = require('mongoose')
+const dbUrl = process.env.MONGO_ATLAS_URL //|| require("./config/config").databaseURL // the right way
+const { app, port } = require('./src/server')
+const dbConfig = require('./config/config')
+
+
+// Kicka igång servern
+const dbOptions = { useUnifiedTopology: true, useNewUrlParser: true }
+mongoose.connect(dbUrl, dbOptions).then(() => {
+    app.listen(port, () => console.log(`App listening on port ${port}!`))
+})
+
+module.exports = { app, port }
